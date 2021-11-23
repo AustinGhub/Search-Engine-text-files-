8b87ee0104cd111732b659f4485da82a15a950a2
AustinGhub <67807368+AustinGhub@users.noreply.github.com>
2021-11-23 03:21:08 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import time
 if __name__ == "__main__":
     start_time = time.time()
     print('-'*8,"LOCAL FILE SEARCH ENGINE", '-'*8)
-    print('-'*8,"AN ICS-33 FINAL PROJECT", '-'*8)
 
     wA.app.run(debug=True)
 
